ba81db15d42a672ae6329c49dd69f5cdbd463352
HyperWinX <artmsmth@gmail.com>
2024-11-18 20:23:33 +0400

    Added config

---
+++ nvim/init.lua
@@ -0,0 +1,15 @@
+-- Setup plugin manager
+require("config.lazy")
+
+-- Custom keybinds
+keybind_opts = {
+  noremap = true,
+  silent = true
+}
+
+vim.api.nvim_set_keymap("n", "<C-t>", ":NvimTreeToggle<CR>", keybind_opts)
+
+-- Configurations
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
